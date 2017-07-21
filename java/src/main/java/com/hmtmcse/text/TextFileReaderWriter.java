f9dc173f3f4d37efb591590f6e563aa6040badba
Touhid Mia <hmtm.cse@gmail.com>
2017-07-21 13:01:15 +0600

    Touhid Mia

--- java/src/main/java/com/hmtmcse/text/TextFileReaderWriter.java
+++ java/src/main/java/com/hmtmcse/text/TextFileReaderWriter.java
@@ -89,6 +89,8 @@ public class TextFileReaderWriter {
         findReplace = findReplace.replaceAll("–","-");
         findReplace = findReplace.replaceAll("…",".");
         findReplace = findReplace.replaceAll("‘","");
+        findReplace = findReplace.replaceAll("“","\"");
+        findReplace = findReplace.replaceAll("”","\"");
         return findReplace;
     }
 
